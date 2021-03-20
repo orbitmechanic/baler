9b170fbf3395e9594227bfc2446d029789954a09
drhongos <lucasziegenfuhs@gmail.com>
2021-03-20 05:39:51 -0300

    Frontend: users, gallery, likes, characterSheet

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Button from 'react-bootstrap/Button'
 //Components
 import LogButton from './components/logButton';
 import MinterSpace from './components/minterSpace';
+import CharacterSheet from './components/characterSheet';
+
 //Helpers
 
 // Extras
@@ -18,15 +20,23 @@ const Web3 = require('web3');
 
 function App() {
   const [user, setUser] = useState(null);
+  //Data on dApp
   const [userData, setUserData] = useState(null);
+  const [gallery, setGallery] = useState(null);
+  const [likes, setLikes] = useState(null);
   // Replace this upon deployment:
   const [contractAddress]= useState("0xf271dF427d16D3f7910A8b2311E7c2f4702aF8C4");
+  //DApp handlers
+  const [nftSelected, setNftSelected] = useState(null);
+  const [likesOpen, setLikesOpen] = useState(true);
+  const [userList, setUserList] = useState(true);
+  const [galleryOwner, setGalleryOwner] = useState(null);
 
-  // Moralis confs. 
+  // Moralis confs.
   // Replace these per server:
   Moralis.initialize("GsvY24d7vRLDtU5ixRYK10Ry7GscQEJP8mDVzzY3");
   Moralis.serverURL = 'https://wvtjjsmhximj.moralis.io:2053/server'
-  
+
 
   //Moralis functions
   const currentUser = Moralis.User.current();
@@ -34,7 +44,10 @@ function App() {
     if(!user){
       setUser(currentUser);
       fetchUserData(currentUser);
+      getGallery(currentUser.attributes.ethAddress);
+      getLikes(currentUser);
       connectContract();
+
     }
   } else {
     console.log('should explain protocol and ask for login');
@@ -56,7 +69,7 @@ function App() {
         console.log(error);
     }
   }
-  
+
   async function logout(){
       console.log('Login out');
       Moralis.User.logOut().then(() => {
@@ -66,6 +79,17 @@ function App() {
       });
     }
 
+  function getProfileForAddress(address) {
+      return address
+        ? Moralis.Cloud.run("getProfileForAddress", { address })
+        : null;
+    }
+  function getNameForAddress(address) {
+        return address
+          ? Moralis.Cloud.run("getNameForAddress", { address })
+          : null;
+    }
+
   //Backend queries
   async function fetchUserData(currentUser){
     let usernameRetrieve = await currentUser.get('username')
@@ -73,6 +97,50 @@ function App() {
     let ethAddress = await currentUser.get('ethAddress')
     await setUserData({username:usernameRetrieve, email:emailRetrieve, address: ethAddress})
   }
+
+
+  async function getGallery(address){
+      // get user profile
+      const targetAddress = address;
+      console.log('address targeted: ',targetAddress)
+      const username = await getNameForAddress(targetAddress);
+      const profile = await getProfileForAddress(targetAddress);
+      console.log('Profile: ',profile);
+      if (profile.pics.length > 0) {
+        setGalleryOwner(username.username)
+        setGallery(profile.pics)
+      } else {
+        alert("No pics from: ",{address});
+      }
+    }
+
+  async function getLikes(user){
+      let query = new Moralis.Query("UserImage");
+      const relation = user.relation('likes')
+      let results = await relation.query().find();
+      const pics = results.map(function (r) {
+        return {
+          url: r.attributes.img.url(),
+          name: r.attributes.img.name(),
+        };
+      });
+      // console.log('likes is ',results)
+      setLikes(pics);
+      }
+
+  async function fetchUserList() {
+    if(userList){
+      setUserList(null);
+      }else{
+      const userAddresses = await Moralis.Cloud.run("getUserList", {});
+        // console.log('other users:',userAddresses);
+      setUserList(userAddresses);}
+    }
+
+
+
+//App handler functions
+
 
   // Chain connections
   async function connectContract() {
@@ -106,35 +174,102 @@ function App() {
           <Button onClick={()=>{console.log('TODO')}}>Set</Button>
         :null}
       </Row>
-      <Row className="flex justify-content-center">
-      {/*title minter*/}
-        <h2>Mint new NFT</h2>
-      </Row>
-      <Row >
           {/*image handler */}
-        <MinterSpace  user={user} />
+      <Row
+        className="flex justify-content-center border"
+        style={{background: 'lightblue',}}
+        onClick={()=>{setNftSelected(null)}}
+              >
+            {/*title minter*/}
+                <h2>Mint new NFT</h2>
       </Row>
-      <Row>
-      {/*Bale it button*/}
+      <Row className='flex border'>
+                {/*image handler */}
+          {nftSelected?
+                <CharacterSheet
+                  user={user}
+                  nftSelected={nftSelected}
+                />
+                :
+                "Minterspace"
+              }
+              <MinterSpace  user={user} />
       </Row>
-      <Row>
+      <Row className=' justify-content-center border' style={{background: 'lightgreen',}}>
       {/*Collection list and character sheet handler */}
-        <Col>
-          {/*Thumb */}
-        </Col>
-        <Col>
-          {/* Metadata */}
-        </Col>
-        <Col>
-          {/*handlers */}
-        </Col>
+        <h3>{galleryOwner} Collection </h3>
+        {gallery?<h3 style={{marginLeft:'5px'}}>> {gallery.length}</h3>:null}
       </Row>
       <Row>
-      {/*Likes list ..*/}
-      </Row>
-      <Row>
-      {/*Users list */}
-      </Row>
+        { gallery?
+          <div className='container center' id='gallery'>
+            {gallery.map(function(d){
+              return(
+                <a onClick={()=>{setNftSelected(d)}}>
+                  <img src={d.url} alt={d.name} height="200" width="200" margin="5px"></img>
+                </a>
+                )
+                })}
+          </div>
+          :
+          <p className='center'>You have no NFTs minted! Do your first!!</p>
+        }
+        </Row>
+        <Row
+          className=' justify-content-center border'
+          style={{background: 'grey',}}
+          onClick={()=>{setLikesOpen(!likesOpen)}}
+          >
+         {/*Likes list ..*/}
+         <h3 className='center'>Likes</h3>
+         </Row>
+         <Row>
+         { likes && likesOpen ?
+           <div>
+            {likes && likes.length == 0?
+
+               <p className='center'>You have to like something..!</p>
+               :
+             <div className='container center' id='gallery'>
+               {likes.map(function(d){
+                 return(
+                   <a onClick={()=>{setNftSelected(d)}}>
+                     <img src={d.url} alt={d.name} height="200" width="200" margin="5px"></img>
+                   </a>
+                   )
+                   })}
+             </div>
+            }
+          </div>
+           :
+           null
+         }
+         </Row>
+         <Row
+           className=' justify-content-center border'
+           style={{background: 'coral',}}
+           onClick={fetchUserList}
+         >
+         {/*Users list */}
+         <h3 className='center'>Other users</h3>
+         </Row>
+         <Row>
+           <div className='center'>
+           {userList && userList.length>0?
+             userList.map((ad)=>{
+               return(
+                 <div>
+                 <a onClick={()=>{
+                   getGallery(ad);
+                 }}>
+                   {ad.slice(0,6)}..
+                 </a><br />
+                 </div>
+               )
+             })
+             :null}
+           </div>
+         </Row>
     </Container>
   );
 }
