86c8428a3519e4544460d6b3bcc0d9d98852954d
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2021-04-14 23:34:40 -0300

    4

--- simpleTestClient/testMain.js
+++ simpleTestClient/testMain.js
@@ -9,10 +9,10 @@ let userEthAddress;
 $(document).ready(async function () {
 
   window.web3 = await Moralis.Web3.enable();
-  baleTestContractInstance = new web3.eth.Contract(testAbi, "0x4dc9d91467b6BE7d3D1D23A73153459eB023ffeB");
+  baleTestContractInstance = new web3.eth.Contract(testAbi, "0x310eA475D410A11DCE7503191D70f0813c432749");
   console.log(baleTestContractInstance);
   user = await Moralis.User.current();
-  userEthAddress = user.attributes.accounts[0];
+  userEthAddress = await user.attributes.accounts[0];
 
   console.log("user:");
   console.log(user);
@@ -36,36 +36,25 @@ async function login() {
     }
 }
 
-let nftAmount = 5;
+let nftAmount = 18;
 
 async function saveNumber() {
   let numberToSave = $("#numberInputField").val();
-  // window.web3 = await Moralis.Web3.enable();
-  // let baleTestContractInstance = new web3.eth.Contract(testAbi, "0x4dc9d91467b6BE7d3D1D23A73153459eB023ffeB");
-  // console.log(baleTestContractInstance);
-
-  // let minted = [];
-
-  let minted = await baleTestContractInstance.methods.mint("0x37009168706F1f766BFeB7B5Ca34e44657Ed6FB4", nftAmount, [2,5]).send({from: ethereum.selectedAddress });
+  
+  // mint to user address
+  let minted = await baleTestContractInstance.methods.mint(userEthAddress, nftAmount, [2,5]).send({from: ethereum.selectedAddress });
   console.log(minted);
-
-  // let answer = [];
-  /*
-  let answer = await baleTestContractInstance.methods.exists(numberToSave).send();
-  console.log(answer);*/
 
   nftAmount = nftAmount +1;
 }
 
 
 async function approvalTesting () {
-  // window.web3 = await Moralis.Web3.enable();
-  // let baleTestContractInstance = new web3.eth.Contract(testAbi, "0x4dc9d91467b6BE7d3D1D23A73153459eB023ffeB");
   console.log(baleTestContractInstance);
 
   let approving = [];
-  
-  approving = await baleTestContractInstance.methods.setApprovalForAll("0x4dc9d91467b6BE7d3D1D23A73153459eB023ffeB", true).send({from: ethereum.selectedAddress });
+  // approve contract as operator
+  approving = await baleTestContractInstance.methods.setApprovalForAll("0x310eA475D410A11DCE7503191D70f0813c432749", true).send({from: ethereum.selectedAddress });
   console.log(approving);
 
 }
